3719811aa4c04c1a24ac6cef99931b161c7bc937
kumarsandeep567 <kumar.sandee@northeastern.edu>
2024-06-26 14:30:22 -0400

    Removed FastSignIn and streamlined UI

--- fe/src/pages/SignIn/SignIn.js
+++ fe/src/pages/SignIn/SignIn.js
@@ -1,13 +1,10 @@
 import { useHistory } from 'react-router-dom'
-//import Page from 'material-ui-shell/lib/containers/Page/Page'
 import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
-//import Button from '@material-ui/Button'
 import Paper from '@material-ui/core/Paper'
-//import MenuContext from 'material-ui-shell/lib/providers/Menu/Context'
 import { Link } from 'react-router-dom'
 
 // save keys to local storage
@@ -60,13 +57,11 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: 'auto',
       marginRight: 'auto',
     },
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(18),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(
-      3
-    )}px`,
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`,
   },
   avatar: {
     margin: theme.spacing(1),
@@ -82,13 +77,13 @@ const useStyles = makeStyles((theme) => ({
   },
   container: {
     display: 'flex',
+    padding: '10px', 
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     height: `100%`,
   },
-  buttonPadding: {    
-    //padding: '30px',  
+  buttonPadding: {  
     marginBottom: '30px', 
   },
 }))
@@ -114,9 +109,6 @@ const SignIn = () => {
   // tokens encode the userid
   function handleSubmit(event) {
     event.preventDefault()
-
-    //console.log(username);
-    //console.log(password);
     const paramdict = {
       'name': username,
       'password': password
@@ -133,9 +125,6 @@ const SignIn = () => {
     console.log(paramdict);
 
     console.log("Signin.js: fetching from ".concat(`${process.env.REACT_APP_API_SERVICE_URL}/login`))
-    // verify user/pwd, get encoded userid as access and refresh tokens in return
-    //fetch("http://localhost:5000/login", config)
-    //fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/login`, config)
     fetch(`${process.env.REACT_APP_API_SERVICE_URL}/login`, config)
       .then(response => response.json())
       .then(data => {
@@ -148,47 +137,6 @@ const SignIn = () => {
 
         // Redirect to Geolocation page
         history.push("/geolocation");
-      })
-      .catch( (err) => {
-        alert(err);
-        console.log(err);
-      });
-  }
-
-  // we submit our tokens and receive
-  // a refreshed a renewed access
-  // token unless the refresh token
-  // has expired
-  function handleFastSignIn() {
-
-    const paramdict = getAuthorisation();
-    const config = {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(paramdict)
-    }
-
-    console.log("Signin.js: fetching from ".concat(`${process.env.REACT_APP_API_SERVICE_URL}/fastlogin`))
-    // verify user/pwd, get encoded userid as access and refresh tokens in return
-    //fetch("http://localhost:5000/fastlogin", config)
-    //fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/fastlogin`, config)
-    fetch(`fastlogin`, config)
-      .then(response => response.json())
-      .then(data => {
-
-        // save to local storage
-        console.log("received these keys in return:")
-        console.log(data);
-        saveAuthorisation({
-          access: data[0][0],
-          refresh: data[0][1],
-        });
-
-        // back to landing page!
-        history.push("/");
       })
       .catch( (err) => {
         alert(err);
@@ -213,22 +161,20 @@ const SignIn = () => {
             {'Sign Out'}
           </Typography> 
           <Typography gutterBottom>If you are not the only one on this device.</Typography>
-          <Button fullWidth variant="contained" margin="normal" color="secondary" onClick={handleSignOut} className={classes.buttonPadding}>
+          <Button 
+            fullWidth 
+            variant="contained" 
+            margin="normal" 
+            color="secondary" 
+            onClick={handleSignOut} 
+            className={classes.buttonPadding}
+          >
             {'Sign Out'}
           </Button>
 
-          <Typography component="h1" variant="h5" className={classes.padding}>
-            {'Fast Sign In'}
-          </Typography>
-          <Typography gutterBottom>If this is your device.</Typography>
-          <Button fullWidth variant="contained" margin="normal" color="primary" onClick={handleFastSignIn} className={classes.buttonPadding}>
-            {'Sign In'}
-          </Button>
-
           <Typography component="h1" variant="h5">
-            {'Password Sign In'}
+            {'Sign In'}
           </Typography>
-          <Typography gutterBottom>You may password-sign-in on any device.</Typography>
           <form className={classes.form} onSubmit={handleSubmit} noValidate>
             <TextField
               value={username}
@@ -276,7 +222,7 @@ const SignIn = () => {
             }}
           >
             <Link to="/password_reset">Forgot Password?</Link>
-            <Link to="/signup">Register</Link>
+            <Link to="/signup">Create an account</Link>
           </div>
         </div>
       </Paper>
